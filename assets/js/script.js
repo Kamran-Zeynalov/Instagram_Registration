6779cf75d2dd40ab77f09a29160134c51369d278
Kamran Z <kamraneze@code.edu.az>
2023-01-26 01:50:10 +0400

    Code tekrari var amma novbeti commitde azaldaram

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let btn = document.getElementById("btn");
+
+btn.onclick = function () {
+    let name = document.getElementById("firstname").value;
+    let surname = document.getElementById("surname").value;
+    let email = document.getElementById("mail").value.indexOf("@");
+    let password = document.getElementById("pass").value;
+
+
+    if (name.length >= 3 && surname.length >= 3 && email != -1 && password.length >7) {
+        window.open(
+            "https://www.google.com", "_blank");
+        return true;
+    }
+    else if(name.length >= 3 && surname.length >= 3 && email == -1 && password.length <=7){
+        document.querySelector(".erorAccount").innerHTML = "This mail or password cannot be obtained. Please try again";
+        return false;
+    }
+
+    else if(name.length < 3 && surname.length < 3 && email == -1 && password.length <=7){
+        document.querySelector(".erorNick").innerHTML = "This name and surname cannot be obtained. Please try again";
+        return false;
+    }
+    else {
+        document.querySelector(".erorNick").innerHTML = "This name and surname  cannot be obtained. Please try again";
+        document.querySelector(".erorAccount").innerHTML = "This mail or password cannot be obtained. Please try again";
+        return false;
+    }
+}
